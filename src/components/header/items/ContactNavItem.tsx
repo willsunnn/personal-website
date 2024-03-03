2d39040c3ead7c1804bd147ce27fba1fdb3d2286
William Sun <sunwilliam@gmail.com>
2024-03-02 17:46:46 -0800

    Cleaned up Contact Nav Item

--- src/components/header/items/ContactNavItem.tsx
+++ src/components/header/items/ContactNavItem.tsx
@@ -3,7 +3,9 @@ import { ExpandableNavItem } from "./NavItem"
 import { Avatar, AvatarFallback, AvatarImage } from "@/shadui/components/ui/avatar"
 import { Link } from "react-router-dom"
 
-const ContactMethod = (props: { link: string, icon: React.ReactNode, value: string }) => {
+type ContactMethodProps = { link: string, icon: React.ReactNode, value: string }
+
+const ContactMethod = (props: ContactMethodProps) => {
     return (<>
         <Link to={props.link} target="_blank" className="flex items-center gap-1">
             {props.icon}
@@ -12,28 +14,35 @@ const ContactMethod = (props: { link: string, icon: React.ReactNode, value: stri
     </>)
 }
 
+const contacts: ContactMethodProps[] = [
+    {
+        link: "mailto:sunwilliam@gmail.com",
+        icon: (<IoMdMail className="mt-[3px]" />),
+        value: "sunwilliam@gmail.com"
+    },
+    {
+        link: "https://linkedin.com/in/willsunnn",
+        icon: (<IoLogoLinkedin className="mt-[3px]" />),
+        value: "willsunnn"
+    },
+    {
+        link: "https://github.com/willsunnn",
+        icon: (<IoLogoGithub className="mt-[3px]" />),
+        value: "willsunnn"
+    },
+]
+
 export const ContactNavItem = () => {
     return <ExpandableNavItem title="Contact">
-        <div className="flex flex-col w-[350px] md:flex-row md:w-[450px] p-6 gap-6">
-            <div className="flex h-full flex-1 select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md pt-6">
-                <div className="mb-2 mt-4 text-lg font-medium">
-                    William Sun
-                </div>
-                <p className="text-sm leading-tight text-muted-foreground">
-                    Backend Software Engineer with experience in RESTful APIs and Event-Driven-Architecture
-                </p>
-            </div>
-
-            <div className="flex flex-row flex-1 md:flex-col p-3 gap-6 items-center">
-                <Avatar className="w-20 h-20">
-                    <AvatarImage src="/profile_picture.JPEG" alt="@shadcn" />
-                    <AvatarFallback>WS</AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col gap-0.5">
-                    <ContactMethod link="mailto:sunwilliam@gmail.com" icon={<IoMdMail className="mt-[3px]" />} value="sunwilliam@gmail.com" />
-                    <ContactMethod link="https://linkedin.com/in/willsunnn" icon={<IoLogoLinkedin className="mt-[3px]" />} value="willsunnn" />
-                    <ContactMethod link="https://github.com/willsunnn" icon={<IoLogoGithub className="mt-[3px]" />} value="willsunnn" />
-                </div>
+        <div className="flex flex-row flex-1 md:flex-col p-3 gap-6 items-center">
+            <Avatar className="w-20 h-20">
+                <AvatarImage src="/profile_picture.JPEG" alt="@shadcn" />
+                <AvatarFallback>WS</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col gap-0.5">
+                {
+                    contacts.map(ContactMethod)
+                }
             </div>
         </div>
     </ExpandableNavItem>
