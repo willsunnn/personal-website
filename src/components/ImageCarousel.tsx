fdc9fe014218c31e55d42f087315eed5563159e3
William Sun <sunwilliam@gmail.com>
2024-03-04 01:19:13 -0800

    Added basic about me section

---
+++ src/components/ImageCarousel.tsx
@@ -0,0 +1,26 @@
+import { Carousel, CarouselContent, CarouselItem } from "@/shadui/components/ui/carousel"
+import { cn } from "@/shadui/lib/utils"
+import Autoplay from "embla-carousel-autoplay"
+
+export type ImageContent = { src: string, className: string }
+
+export const ImageCarousel = (props: { className: string, images: ImageContent[] }) => {
+  return <Carousel
+    orientation="vertical"
+    plugins={[
+      Autoplay({
+        delay: 2000,
+        stopOnInteraction: false,
+      }),
+    ]}
+  >
+    <CarouselContent className={cn(props.className, "m-0 p-0")}>
+      {
+        props.images.map((image) => (
+          <CarouselItem className={cn("w-full h-full", "m-0 p-2")}>
+            <img src={image.src} className={cn("w-full h-full", image.className, "rounded-lg")} />
+          </CarouselItem>))
+      }
+    </CarouselContent>
+  </Carousel>
+}
--- src/pages/home/AboutMeContent.tsx
+++ src/pages/home/AboutMeContent.tsx
@@ -1,7 +1,39 @@
+import { ImageCarousel } from "@/components/ImageCarousel"
+
 export const AboutMeContent = () => {
   return (
-    <>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue lacus viverra vitae congue eu consequat ac felis. Vel pharetra vel turpis nunc eget. Eget est lorem ipsum dolor sit. Donec pretium vulputate sapien nec sagittis. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Libero justo laoreet sit amet cursus sit. Nisl purus in mollis nunc sed id semper risus in. Sociis natoque penatibus et magnis dis. Sit amet cursus sit amet. Sagittis orci a scelerisque purus. Maecenas accumsan lacus vel facilisis volutpat est. Sed id semper risus in hendrerit gravida.
-    </>
+    <div className="flex flex-col gap-6 content-center">
+
+      <h1 className="text-4xl font-semibold">About Me</h1>
+      <p>I'm a backend software engineer, with some personal experience working on frontend</p>
+      <p>My backend experience includes designing and implementing REST APIs and GraphQL APIs, as well as designing Microservices in an event-driven architecture</p>
+
+      <h2 className="text-2xl font-semibold">Skills</h2>
+      <p>I'm a backend software engineer, with some personal experience working on frontend</p>
+      <p>My backend experience includes designing and implementing REST APIs and GraphQL APIs, as well as designing Microservices in an event-driven architecture</p>
+
+      <div className="w-2/3 max-w-[900px] min-w-[600px] h-[400px] flex flex-row gap-12 self-center">
+        <ImageCarousel className="w-[300px] h-[400px]" images={[
+          {
+            src: "/home/about/Graduation-1.jpg",
+            className: ""
+          },
+          {
+            src: "/home/about/Graduation-2.jpg",
+            className: ""
+          },
+          {
+            src: "/home/about/Graduation-3.jpg",
+            className: "object-cover"
+          }
+        ]} />
+        <div className="flex-1 h-full flex flex-col justify-center gap-4">
+          <h2 className="text-2xl font-semibold">Education</h2>
+          <p>I graduated summa cum laude from University of California Irvine with a Bachelor's in Computer Science and Engineering.</p>
+          <p>Coursework includes Data Structures, Algorithms, Databases, Embedded Software, Internet of Things, Compilers, and Operating Systems.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
