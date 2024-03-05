b0696bae0df780151360d44364c2744fe626c0b2
William Sun <sunwilliam@gmail.com>
2024-03-04 21:54:39 -0800

    Added Work Experience section

---
+++ src/components/BulletedItem.tsx
@@ -0,0 +1,102 @@
+import { cn } from "@/shadui/lib/utils"
+import { HiExternalLink } from "react-icons/hi"
+import { OptionalLink } from "./OptionalLink"
+import { Link } from "react-router-dom"
+
+export type StartConnector = "connect" | "no_connect"
+export type EndConnector = "connect" | "no_connect" | "ellipsis_connect"
+
+export type TitleAndTime = {
+    title: string,
+    range: string
+}
+
+export type BulletedItem = React.PropsWithChildren<{
+    name: string,
+    link: string,
+    titles: TitleAndTime[],
+    location: string,
+    description: string[],
+}>
+
+export type BulletedItemProps = BulletedItem & {
+    startConnector: StartConnector,
+    endConnector: EndConnector,
+}
+
+export const determineConnectors = (items: BulletedItem[]): BulletedItemProps[] => {
+    return items.map((item, index, arr) => ({
+        ...item,
+        startConnector: (index == 0) ? "no_connect" : "connect",
+        endConnector: (index == arr.length - 1) ? "no_connect" : "connect"
+    }))
+}
+
+export const BulletedItem = (props: BulletedItemProps) => {
+    const startConnectorVisibility = props.startConnector === "connect" ? "visible" : "invisible"
+    const endConnectorVisibility = props.endConnector === "connect" ? "visible" : "invisible"
+    return (
+        <div className="flex flex-row gap-2">
+            {/* The dots and connectors on the left side*/}
+            <div className="flex flex-col w-6 h-page">
+                <div className={cn("bg-black h-2 w-0.5 ml-1.5 -mb-0.5", startConnectorVisibility)} />
+                <svg className="w-full h-3.5">
+                    <circle cx="7" cy="7" r="6" stroke-width="2" stroke="black" fill="none" className="w-4 h-4" />
+                </svg>
+                <div className={cn("bg-black flex-1 w-0.5 ml-1.5 -mt-0.5", endConnectorVisibility)} />
+            </div>
+
+            {/* The content of the bulleted item*/}
+            <div className="flex flex-col pt-0 pb-16 w-full">
+
+                {/* The name & location of the position */}
+                <div className="flex flex-row">
+                    <Link to={props.link} target={"_blank"} className="hover:underline flex flex-row gap-1 items-center">
+                        <h2 className="text-lg font-semibold">{props.name}</h2>
+                        <HiExternalLink className="-mt-1" />
+                    </Link>
+                    <div className="flex-1" />
+                    <div>{props.location}</div>
+                </div>
+
+                {/* The titles of each position */}
+                <div className="pl-4 flex flex-col pt-3">
+                    {
+                        props.titles.map((title) => {
+                            return (
+                                <div className="flex flex-row">
+                                    <div className="flex-1 text-sm font-semibold">{title.title}</div>
+                                    <div className="text-sm">{title.range}</div>
+                                </div>)
+                        })
+                    }
+                </div>
+
+                {/* The description */}
+                <div className="pl-4 flex flex-col pt-3">
+                    {
+                        props.description.map((line) => {
+                            return (
+                                <div className="flex flex-row content-start items-start justify-start">
+                                    <svg className="w-2 h-2 mt-2 mr-2">
+                                        <circle cx="2" cy="2" r="2" fill="black" className="w-2 h-2" />
+                                    </svg>
+                                    <div className="text-sm">{line}</div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+            </div>
+        </div >
+    )
+}
+
+export const BulletedItemList = (props: { items: BulletedItemProps[] }) => {
+    return <div className="flex flex-col">
+        {
+            props.items.map(BulletedItem)
+        }
+    </div>
+}
--- src/pages/home/WorkExperienceContent.tsx
+++ src/pages/home/WorkExperienceContent.tsx
@@ -1,7 +1,77 @@
+import { BulletedItem, BulletedItemList, determineConnectors } from "@/components/BulletedItem"
+
+const workExperience: BulletedItem[] = [
+  {
+    name: "Project Ronin",
+    link: "https://www.projectronin.com/",
+    location: "San Mateo, CA",
+    titles: [
+      {
+        title: "Software Engineer",
+        range: "November 2023 - February 2024"
+      },
+    ],
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ]
+  },
+  {
+    name: "CalAmp",
+    link: "https://www.calamp.com/",
+    location: "Irvine, CA",
+    titles: [
+      {
+        title: "Software Engineer II",
+        range: "July 2023 - November 2023"
+      },
+      {
+        title: "Software Engineer I",
+        range: "April 2022 - July 2023"
+      },
+      {
+        title: "Assosciate Software Engineer",
+        range: "September 2021 - January 2022"
+      },
+      {
+        title: "Software Engineer Intern",
+        range: "June 2021 - September 2021",
+      },
+    ],
+    description: [
+      "Worked on both RESTful and Event‑Driven backend microservices built using Java Spring and Node.js",
+      "Improved performance of a bottlenecking backend microservice, increasing throughput by 3 times",
+      "Created and maintained pipelines for deployments in AWS EKS and Lambdas.Added integrations with AWS Secrets Manager",
+      "Other notable tools used include PostgreSQL, Redis, MongoDB, S3, Kafka, ActiveMQ, and Kinesis",
+    ]
+  },
+  {
+    name: "Samaritan",
+    link: "https://www.samaritan.city/",
+    location: "Seattle, WA",
+    titles: [
+      {
+        title: "Android Developer Intern",
+        range: "May 2020 - September 2020"
+      },
+    ],
+    description: [
+      "Updated Samaritan’s user‑facing Android app, bringing 2 years worth of iOS updates to the Android version",
+      "Created and refactored many different core activities on the app, including the app’s primary newsfeed and messaging pages",
+      "Learned and used Functional Programming, Asynchronous handling of API calls, the Agile process model and the different mobile software architectures",
+    ]
+  }
+]
+
 export const WorkExperienceContent = () => {
   return (
-    <>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue lacus viverra vitae congue eu consequat ac felis. Vel pharetra vel turpis nunc eget. Eget est lorem ipsum dolor sit. Donec pretium vulputate sapien nec sagittis. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Libero justo laoreet sit amet cursus sit. Nisl purus in mollis nunc sed id semper risus in. Sociis natoque penatibus et magnis dis. Sit amet cursus sit amet. Sagittis orci a scelerisque purus. Maecenas accumsan lacus vel facilisis volutpat est. Sed id semper risus in hendrerit gravida.
-    </>
+    <div className="flex flex-col space-y-16 content-center">
+      <div className="w-2/3 max-w-[900px] min-w-[600px] self-center flex flex-col gap-6">
+        <h1 className="text-4xl font-semibold">Work Experience</h1>
+        <BulletedItemList items={determineConnectors(workExperience)} />
+      </div>
+    </div>
   )
 }
